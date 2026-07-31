cd5041cd0d36ec45dff374fc63bb6817bd10a2c4
HNWebTechnologies <hnwebtechnologies26@gmail.com>
2026-07-31 23:15:58 +0530

    chore(seo): canonicalize domain to https://www.hnwebtechnologies.com in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://hnwebtechnologies.com';
+  const baseUrl = 'https://www.hnwebtechnologies.com';
 
   return [
     {
