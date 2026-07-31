e3bc1ba90f3ae0f80e726ae9bc9868d88ac94d57
HNWebTechnologies <hnwebtechnologies26@gmail.com>
2026-07-31 23:11:21 +0530

    chore(seo): canonicalize domain to https://www.hnwebtechnologies.com in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://hnwebtechnologies.com';
+  const baseUrl = 'https://www.hnwebtechnologies.com';
 
   return {
     rules: {
@@ -9,6 +9,8 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
+    // Include Host header for canonical domain and sitemap pointing to www
+    host: baseUrl,
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
