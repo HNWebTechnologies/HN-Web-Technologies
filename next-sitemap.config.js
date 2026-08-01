147769eff84f38888ad38db142fbc44f25cf5ea4
HNWebTechnologies <hnwebtechnologies26@gmail.com>
2026-08-01 11:21:53 +0530

    chore: add robots.txt and generate sitemap at build

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** next-sitemap.config.js */
+module.exports = {
+  siteUrl: 'https://www.hnwebtechnologies.com',
+  generateRobotsTxt: false,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
