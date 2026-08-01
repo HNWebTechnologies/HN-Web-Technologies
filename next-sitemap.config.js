ab74b1292d2c324606479bd2e2e0af00df3b781a
HNWebTechnologies <hnwebtechnologies26@gmail.com>
2026-08-01 11:56:10 +0530

    fix: remove next-sitemap.config.js (using native Next.js sitemap.ts instead)

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** next-sitemap.config.js */
-module.exports = {
-  siteUrl: 'https://www.hnwebtechnologies.com',
-  generateRobotsTxt: false,
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
-};
